62e1089eb9f88412d9c8075ea56311e6b112d06b
Leopc1977 <leophancaowz@gmail.com>
2021-12-03 17:15:51 +0100

    yes

--- day_3/secondStar.py
+++ day_3/secondStar.py
@@ -1,10 +1,16 @@
-rateOxygen = []
-rateCarbon = []
+#https://stackoverflow.com/questions/489999/convert-list-of-ints-to-one-number
+def magic(numList):         # [1,2,3]
+    s = map(str, numList)   # ['1','2','3']
+    s = ''.join(s)          # '123'
+    #s = int(s)              # 123
+    return s
+
+rateOxygen = 0
+rateCarbon = 0
 
 total = 0
 lines = []
-with open('input.txt') as file:
-
+with open('AoC\day_3\input.txt') as file:
     for line in file:
         lstNumber = []
         for number in line:
@@ -20,21 +26,44 @@ def getMaxFromColumnANDFilter(lines, lstLineIndex,currentColumn):
     for line in lstLineIndex:
         lstQteBit[lines[line][currentColumn]]+=1
 
-    maxi = int(lstQteBit[1]>lstQteBit[0])
-
+    maxi = int(lstQteBit[1]>=lstQteBit[0])
+    lstGoodOnes=[]
     #filter good ones and return it
-    for i in range(len(lstLineIndex)-1,0,-1):
+    for i in range(len(lstLineIndex)):
         indexLine = lstLineIndex[i]
-        if lines[indexLine][currentColumn]!=maxi:
-            lstLineIndex.remove(indexLine)
-
-    return lstLineIndex
+        if lines[indexLine][currentColumn]==maxi:
+            lstGoodOnes.append(indexLine)
+    return lstGoodOnes
 
 lstLineIndex = [i for i in range(len(lines))]
-
 currentColumn = 0
 while len(lstLineIndex)>1:
     lstLineIndex = getMaxFromColumnANDFilter(lines, lstLineIndex, currentColumn)
-    currentColumn+=1
-    
-print(lines[lstLineIndex[0]])
+    currentColumn+=1   
+
+rateOxygen= int(magic(lines[lstLineIndex[0]]),2)
+
+def getMinFromColumnANDFilter(lines, lstLineIndex,currentColumn):
+    #getMax
+    lstQteBit = [0,0]
+    for line in lstLineIndex:
+        lstQteBit[lines[line][currentColumn]]+=1
+
+    mini = int(lstQteBit[1]<lstQteBit[0])
+    lstGoodOnes=[]
+    #filter good ones and return it
+    for i in range(len(lstLineIndex)):
+        indexLine = lstLineIndex[i]
+        if lines[indexLine][currentColumn]==mini:
+            lstGoodOnes.append(indexLine)
+    return lstGoodOnes
+
+lstLineIndex = [i for i in range(len(lines))]
+currentColumn = 0
+while len(lstLineIndex)>1:
+    lstLineIndex = getMinFromColumnANDFilter(lines, lstLineIndex, currentColumn)
+    currentColumn+=1   
+
+rateCarbon= int(magic(lines[lstLineIndex[0]]),2)
+
+print(rateOxygen*rateCarbon)
